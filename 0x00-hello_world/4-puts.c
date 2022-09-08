55a283c4f7475fe6c4cc14ba1f69566b2697bc22
zele27 <nkzele@gmail.com>
2022-09-08 14:31:41 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+/**
+
+ * main - prints a text
+
+ * Return: always 0 (success)
+
+ */
+
+
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
