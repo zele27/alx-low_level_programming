e836241b0ceebf1f8471cf975c23c4bd052ef8e7
zele27 <nkzele@gmail.com>
2022-09-08 14:47:35 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
 
-
-
-/**
-
- * main - prints a text
-
- * Return: always 0 (success)
-
- */
-
-
+//**
+*main 
+* main - prints a text
+* Return: always 0 (success)
+*/
 
 int main(void)
-
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
-
+       puts("\"Programming is like building a multilingual puzzle");
+       return (0);
 }
