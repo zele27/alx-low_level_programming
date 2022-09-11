e3dadfcdc5ae042908e0dbe76da69a551ab4e6cc
zele27 <nkzele@gmail.com>
2022-09-11 15:42:50 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print if the number is positive, zero, or negative
+ * this program prints " Programming is psitive,zero,or negative
+ * Return: 0
+ * *
+ * int main(void)
+ * {
+ * int i;
+ * for(i = 0;< 10; i++)
+ * }
+ * printf("\n");
+ * return(0);
+ * }
+ *
