610d74ba1c093eb165ab27c9495f92322af940e9
zele27 <nkzele@gmail.com>
2022-09-11 09:17:11 -0700

    alphabet

--- 2-print_alphabet.c
+++ 2-print_alphabet.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet.
- *
- * Return: Always 0 (Success)
- */
+ * main -Prints the alphabet.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
 	for (i = 0; i < 26; i++)
 	{
-		putchar(alp[i]);
-	}
-	putchar('\n');
-	return (0);
+		putchar('\n');
+echo u > undoing
+ { 
+	putchar(alph[i]);
 }
-
+putchar('\n');
+return (0);
+}
+:wq
