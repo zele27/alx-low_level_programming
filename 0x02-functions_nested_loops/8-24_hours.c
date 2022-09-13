d7e0dec359cbd6c69cb44334d009bd197b87650b
zele27 <nkzele@gmail.com>
2022-09-13 15:56:57 -0700

    Lever

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,6 +17,7 @@ void jack_bauer(void)
 			_putchar('0' + (hours / 10));
 			_putchar('0' + (hours % 10));
 			_putchar(':');
+			_putchar('0' + (min / 10));
 			_putchar('0' + (min % 10));
 			_putchar('\n');
 			min++;
