9765d763ecc077158e67248cffa179ee23e76826
zele27 <nkzele@gmail.com>
2022-09-13 16:36:22 -0700

    functions

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,28 +9,27 @@ void times_table(void)
 	int i, j, r, f, s;
 
 	r = 0;
-
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 
-			r = i * j;
+		r = i * j;
 			if (j > 0)
 			{
-				_putchar(',');
-				_putchar(',');
-				if (r >= 10 && r < 100)
+			_putchar(',');
+			_putchar(',');
+			if (r >= 10 && r < 100)
 				{
-					f = r / 10;
-					s = r % 10;
-					_putchar('0' + f);
-					_putchar('0' + s);
+				f = r / 10;
+				s = r % 10;
+				_putchar('0' + f);
+				_putchar('0' + s);
 				}
 				else if (r < 10)
 				{
-					_putchar(' ');
-					_putchar('0' + r);
+				_putchar(' ');
+				_putchar('0' + r);
 				}
 			}
 			else
