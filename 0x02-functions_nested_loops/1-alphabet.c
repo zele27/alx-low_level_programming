eb01d8c02a838512c049295f8d7da3619db9b5d2
zele27 <nkzele@gmail.com>
2022-09-13 14:21:04 -0700

    insomia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabets in lower case folowed by a new line
+ */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+
+}
+
