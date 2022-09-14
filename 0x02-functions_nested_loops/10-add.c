b1ed626b53686c436a5f81766ce71537cc3b242f
zele27 <nkzele@gmail.com>
2022-09-13 17:04:08 -0700

    interger

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+
+#include "main.h"
+
+/**
+ * add - adds two intergers
+ * @a: number 1 to be added
+ * @b: number 2 to be added
+ * Return: The reult of the addition
+ */
+int add(int a, int b)
+{
+	return(a + b);
+}
+
