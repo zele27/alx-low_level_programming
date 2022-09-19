acb24938a90469524fc976fc8bbcf1cfaf46e535
zele27 <nkzele@gmail.com>
2022-09-19 10:28:21 -0700

    Battery

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,14 @@
-#include " main.h"
+#include  "main.h"
 
 /**
- * reset_to_98 - updates the value it points to 98.
- * @n the parameter to be updated
- * */
-
+ * reset_to_98 - updates the value of points n to 98
+ *
+ * @n: input
+ * Return: void
+ */
 void reset_to_98(int *n)
 {
-*n = 98;
+	*n 98;
 }
+
 
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,49 +1,19 @@
-#ifndef _HOLBERTON
-#define _HOLBERTON
+#ifndef MAIN_H
+#define MAIN_H
+
 
 int _putchar(char c);
-
-/**
- * reset_to(* - updates the value it points to to 98.
- * @n: the parameter to be updated
- */
-
 void reset_to_98(int *n);
-
-/**
- * swap_int - swaps the values of two integers
- * @a: argument one
- * @b: argument two
- * */
-
 void swap_int(int *a, int *b);
-
-/**
- * _strien - conts the length of a string
- * @s: a string to be counted
- * Return: The length of the string
- */
-
-int _srien(char *s);
-
-/**
- * puts2 - prints every other character
- * @str: the string
- */
-
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
 void puts2(char *str);
-
-/**
- * puts_half - prints half of a string followed by a new line
- * @str: the argument
- */
-
 void puts_half(char *str);
-
-
-void print_array(int *a,int n);
-char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
+char *_strpy(char *dest, char *src);
 int _atoi(char *s);
+
 
 #endif
 
