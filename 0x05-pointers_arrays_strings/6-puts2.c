5f896b412444a88e9448a3fe2f3b7f25d0a2c448
zele27 <nkzele@gmail.com>
2022-09-19 13:55:01 -0700

    Pointers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include <stdi.h>
+#include "main.h"
+
+/**
+ * puts2 -prints every other character
+ * @str: String
+ *
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while(*(str + i) != '\0')
+	{
+		if (i % 2 == 0)
+			putchar(*(str + i));
+		i++;
+	}
+	putchar(10);
+	
+}
+
