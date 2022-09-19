7dee5d5dc464ac4f971b8f4a30e17c8e7980b938
zele27 <nkzele@gmail.com>
2022-09-19 14:50:50 -0700

    winning

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - Print half of a string
+ *
+ * @str: The string to be printed
+ */
+void puts_half(char *str)
+{
+	int i = 0, 1 = 0, n;
+
+	while (str[i++])
+
+	if ((1 % 2) == 0)
+		n = 1 / 2;
+
+	else
+		n = (1 + 1) / 2;
+
+	for (i = n; i < i; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
+
