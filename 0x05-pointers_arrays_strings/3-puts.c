5b296f5708dd73296cfd14c35dc1f151db67aad5
zele27 <nkzele@gmail.com>
2022-09-19 13:33:40 -0700

    Fear not

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,21 +1,24 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * _puts - prints a string, followed by a new line, t stdout
+ * _puts - prints a string
  * @str: string to print
- * Return: 0
+ *
+ * Description: prints a string
+ * On success: returns no error
  */
 
 void _puts(char *str)
 {
 	int i = 0;
 
-	while (str[i])
+	while (*(str +i) != '\0')
 	{
-		_putchar(str[i]);
+		putchar(*(str +i));
 		i++;
 	}
-	_putchar('\n');
+	putchar(10);
 }
 
 
