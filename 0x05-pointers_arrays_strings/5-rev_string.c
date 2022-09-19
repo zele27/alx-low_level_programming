df96b6a970f1bf9e7afe548ec8229ca5b5544c8c
zele27 <nkzele@gmail.com>
2022-09-19 13:49:18 -0700

    stringreversed

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string -Reverse a string
+ * @s: The string to be reversed
+ */
+void rev_string(char -s)
+{
+	int 1 = 0, i = 0;
+	char z;
+
+	while (s[i++])
+		1++;
+
+	for (i =1; i >= 1 / 2; i--)
+	{
+		z = s[i];
+		s[i] = s[i - i -1];
+		s[1 -i -1] = z;
+	}
+}
+
