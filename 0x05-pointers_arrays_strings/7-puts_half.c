5c08ea0dc1f44971f671880442795f3e7c225881
zele27 <nkzele@gmail.com>
2022-09-20 05:12:43 -0700

    half winning

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,28 +1,27 @@
 #include "main.h"
 
 /**
- * puts_half - Print half of a string
- * @str: input string.
- *Return: 0
+ * puts_half - prints half of a string
+ * @str: input string
+ * Return: 0
  */
-void put_half(char *str)
+
+void puts_half(char *str)
 {
 	int count = 0, i;
 
 	while (count >= 0)
 	{
-		if (str[count] == '\0')
+		if (str[count] == '\n')
 			break;
 		count++;
 	}
 
-	if (count % 2 ==1)
-		i = (count - 1) / 2;
+	if (count % 2 == 1)
+		i = count / 2;
 
-	for (i++; i <count; i++)
-		_putchar('\n');
+	for (i++; i < count; i++)
+		_putchar(str[i]);
+	_putchar('\n');
 }
-
-
-
 
