d0afcc619fa8de76ac6be1f701823907a3717a3b
zele27 <nkzele@gmail.com>
2022-09-20 15:34:12 -0700

    Winner

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,35 +1,21 @@
 #include "main.h"
+#include <string.h>
 
 /**
- * puts_half - prints half of a string
- * @str: The string to be printed
- *
- * Return: 0
+ * puts_half -> puts half
+ * @str: string param
  */
 void puts_half(char *str)
 {
-	int j = 0;
-	int k;
+	int x, y, i;
 
-	while (str[j] != '\0')
-	{
-		j++;
-	}
-	if (j % 2 == 1)
-	{
-		k = (j - 1) / 2;
-		k += 1;
-	}
+	x = strlen(str);
+	if (x % 2 == 1)
+	    y = x / 2 + 1;
 	else
-	{
-		k = j / 2;
-	}
-	for (; k < j; k++)
-	{
-		_putchar(str[k]);
-	}
-
-	_putchar(*\n');
+		y =x / 2;
+	for (i = y; i < X; i++)
+	     _putchar(str[i]);
+	_putchar('\n');
 }
-
 
