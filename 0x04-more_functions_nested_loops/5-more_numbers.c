103bd63ea899380761b3af02b98cdc0d1989641b
zele27 <nkzele@gmail.com>
2022-09-22 16:34:40 -0700

    universal language

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,24 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * more_numbers - prints 10 times the numbers, from 0 to 14
- * follwed by a new line
- * Return: 0
+ * more_numbers - prints 01234567891011121314\n x 10
+ * Return: void
  */
-
 void more_numbers(void)
 {
-int i, j;
+char a, b, c, i;
 for (i = 0; i < 10; i++)
 {
-for (j= 0; j < 15; j++)
+for (c = 0; c <= 14; c++)
 {
-if (j >= 10)
-_putchar(j / 10 + '0');
-_putchar(j % 10 + '0');
+if ( c < 10)
+b = c;
+else
+{
+a =c / 10; b = c % 10;
+-putchar('0' + a);
+}
+_putchar('0' +b);
 }
 _putchar('\n');
 }
