745fb590f63f43a6692cab9e8925e8fbb5ecc3e7
zele27 <nkzele@gmail.com>
2022-09-22 05:25:17 -0700

    I suspect people of plotting to make me happy

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,12 +11,15 @@ void reverse_array(int *a, int n)
 int i, j, tmp;
 
 j = n - 1;
-for (i = 0, i < n / 2; i++)
+for (i = 0; i < n / 2; i++)
 {
 tmp = a[i];
 a[i] = a[j];
 a[j] = tmp;
 j--;
 }
+}
+
+return (0);
 }
 
