7200ba11ae6e2a3a933f5d6ae57971de81f0a72b
zele27 <nkzele@gmail.com>
2022-09-21 17:35:33 -0700

    Tasks

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,13 @@
 
 /**
  * _isupper - checks for uppercase character
- * @c: variable character
+ * @c: input character
  * Return: 1 if c is uppercase - 0 otherwise
  */
 
 int _isupper(int c)
 {
-	if (( >= 'A') && (c <= 'Z'))
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
