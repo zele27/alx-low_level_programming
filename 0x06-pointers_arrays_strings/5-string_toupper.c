243e72380cc9cf169572e48ea842a3410ebb8920
zele27 <nkzele@gmail.com>
2022-09-22 09:08:43 -0700

    Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _string_toupper - changes all lowercase letters of a string to uppercase
+ * string_toupper - changes all lowercase letters of a string to uppercase
  * @s: string to modify
  *
  * Return: s modified
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string to use
+ *
+ * Return: string
+ */
+
+char *cap_string(char *s)
+{
+int i = 1,j, check;
+
+char a[] = {',',',',',''!',''','(' ,')',
+
+
+
