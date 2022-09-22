97179fedf300c194ab7788e115b95a5257ff41f1
zele27 <nkzele@gmail.com>
2022-09-22 10:15:59 -0700

    Mozart

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,15 +12,15 @@ int i = 0, j = 0;
 
 char array_leet[] = {'4', '3', '1', '0', '7'};
 
-char array_up[] = {'A', 'E', 'L', '0', 'T'};
+char array_up[] = {'A', 'E', 'L', 'O', 'T'};
 
-char array_low[] = { 'a', 'e', 'l', '0', 't'};
+char array_low[] = { 'a', 'e', 'l', 'o', 't'};
 
 while (s[i] != '\0')
 {
 for (j = 0; j < 5; j++)
 {
-if (s[i] == array_low[j] || s[i] == array_upe[j])
+if (s[i] == array_low[j] || s[i] == array_up[j])
 s[i] = array_leet[j];
 }
 i++;
