c00a54850eb4df347db91fffaf12e56eafdfce9b
zele27 <nkzele@gmail.com>
2022-09-22 17:43:00 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -21,11 +21,11 @@ else if ( i % 5 == 0 && i % 3 != 0)
 {
 printf("Buzz");
 }
-else if (i % 3 ==0 && i % 5 == 0)
+else if (i % 3 == 0 && i % 5 == 0)
 {
 printf("FizzBuzz");
 }
-else if ( i ==1)
+else if ( i == 1)
 {
 printf("%d", i);
 }
@@ -36,6 +36,6 @@ printf(" %d", i);
 }
 printf("\n");
 
-return(0);
+return (0);
 }
 
