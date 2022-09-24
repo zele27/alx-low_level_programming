d460212df785c693b7f3691bbccbe637b556296b
zele27 <nkzele@gmail.com>
2022-09-23 18:40:18 -0700

    sqyuare

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,20 +7,19 @@
 
 void print_square(int size)
 {
+int width, height;
 
 if (size <= 0)
-{
 _putchar('\n');
-}
 else
 {
-int i, j;
-
-for (i =0; i< size; i++)
+for (height = 0; height < size; height++)
 {
+for (width = 0; width < size; width++)
+
 _putchar('#');
-}
 _putchar('\n');
+
 }
 }
 }
