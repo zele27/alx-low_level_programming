706184ad1604725527637aa485b415b4d6a88fec
zele27 <nkzele@gmail.com>
2022-09-23 19:22:01 -0700

    numericals

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,7 +12,7 @@ return (n >= '0' && n <= '9');
 
 /**
  * _atoi - convert a string to an integer
- * @s: string
+ * *@s: string
  * Return: num
  */
 int _atoi(char *s)
@@ -29,7 +29,7 @@ for (i = 0; s[i] != '\0'; i++)
 {
 if (is_numerical(s[i]))
 {
-number = (s[i] - 48) + number *10;
+number = (s[i] - 48) + number * 10;
 
 if (s[i] == ' ')
 break;
