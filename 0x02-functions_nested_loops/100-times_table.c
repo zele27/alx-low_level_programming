552eeac4b9329910f9feab734dbc77d46fd025eb
zele27 <nkzele@gmail.com>
2022-09-23 20:24:41 -0700

    timetable

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,18 +10,18 @@
 void print_times_table(int n)
 {
 
-int a =0, rep, b;
+int a = 0, rep, b;
 
 if (n < 0 || n > 15)
 return;
 
-while(a <= n)
+while (a <= n)
 {
 for (b = 0; b <= n; b++)
 {
-rep = a *b;
+rep = a * b;
 if (b == 0)
-_putchar( '0' + rep);
+_putchar('0' + rep);
 else if (rep < 10)
 {
 _putchar(' ');
