dbe909811551d634790279b4efc1ba27f71e29a1
zele27 <nkzele@gmail.com>
2022-09-26 13:43:55 -0700

    Task

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,10 +2,9 @@
 
 /**
  * _strstr - locates a substring
- * @haystack: string in which to check fr needle
+ * @haystack: string in which to check for needle
  * @needle: substring to find in haystack
- *
- * Return: pointer to begining of needle in haystack or NULL if no match
+ * Return: pointer to beginning of needle in haystack or NULL if no match
  */
 char *_strstr(char *haystack, char *needle)
 {
@@ -30,4 +29,5 @@ return (haystack + i);
 }
 return (0);
 }
+
 
