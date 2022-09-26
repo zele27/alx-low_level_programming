4df48d0c062def1b5ed35a5684839d8778dd5c10
zele27 <nkzele@gmail.com>
2022-09-26 03:56:56 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,13 +2,13 @@
 
 /**
  * _memcpy - input
- * @dest: input pinter to string destination
+ * @dest: input pointer to string destination
  * @src: input pointer to source string
  * @n: number of bytes
  * Return: pointer to destination string
  */
 
-char *_memcpy(char *dest, char *src,unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i = 0;
 
