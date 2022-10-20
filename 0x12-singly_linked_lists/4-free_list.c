7905c0169ff8526e59657cd0e96f6533edfe53ea
zele27 <nkzele@gmail.com>
2022-10-20 06:39:25 -0700

    Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ *free_list - frees memory from list
+ *@head: pointer to the list
+ *Author: Onyiana
+ */
+void free_list(list_t *head)
+{
+list_t *temp = 0;
+
+while (head)
+{
+temp = head->next;
+free(head->str);
+free(head);
+head = temp;
+}
+}
+
